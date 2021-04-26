ed80907f914c13092148ff845c065bca31c4af6a
Valeri Hristov <valerimagic@gmail.com>
2021-04-26 22:40:14 +0300

    update

---
+++ Meeting/DTO/Convert/LineChart/Hover.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO.Convert.LineChart
+{
+    public class Hover
+    {
+        public int sizeOffset { get; set; }
+
+        public Hover()
+        {
+            sizeOffset = 4;
+        }
+
+    }
+}
